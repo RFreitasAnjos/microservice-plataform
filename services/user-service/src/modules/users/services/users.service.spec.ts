c8b25bf275011d797da2b9656a37f97fd099a6e2
Renan Freitas <renan.freitas.anjos@gmail.com>
2026-03-17 03:33:10 -0300

    refactor: removed payment-services and refactor all code of user-service

--- services/user-service/src/modules/users/services/users.service.spec.ts
+++ services/user-service/src/modules/users/services/users.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { UsersRepository } from '../repositories/users.repositories';
+import { PrismaService } from 'src/database/database.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, UsersRepository, PrismaService],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
